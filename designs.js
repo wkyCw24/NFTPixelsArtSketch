2577938afd685733ff67df750d2f82893b63b15a
Winkycw <102515158+wkyCw24@users.noreply.github.com>
2022-08-09 16:17:51 +0800

    Add files via upload

---
+++ designs.js
@@ -0,0 +1,32 @@
+//select color input
+const color = document.getElementById("colorPicker");
+//select size input
+const table = document.getElementById("pixelCanvas");
+const height = document.getElementById("inputHeight");
+const width = document.getElementById("inputWidth");
+let removeGridBtn = document.getElementById("remove-grid");
+
+//when size is submitted by the user, call makeGrid()
+$('#sizePicker').submit(function (event) {
+  event.preventDefault();
+  makeGrid(height, width);
+});
+
+//the makeGrid() function
+function makeGrid(height, width) {
+  for(let i = 0; i < height.value; i++){
+    let row = table.insertRow(i);
+    for(let j = 0; j < width.value ; j++){
+      let cell = row.insertCell(j);
+      cell.addEventListener("click", function(event) {
+        cell.style.backgroundColor = color.value;
+      }
+      );
+    }
+  }
+}
+
+//Remove Grid
+removeGridBtn.addEventListener("click", () => {
+  table.innerHTML = "";
+});
